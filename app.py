bcc28e303dceb59c1bcecc945c11443e520dbc3f
Hendrik Peter van der Meulen <peter@picoreclame.nl>
2019-07-17 08:53:09 +0000

    Some small stuff

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        Hello World
-    </body>
-    </html>
-    '''
+  return 'Hello'
 
 number = 0
 
@@ -30,5 +20,13 @@ def ping():
 def api():
     return jsonify({"hello": "there"})
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+@app.route('/foo/<name>')
+def foo(name):
+    return render_template('index.html', to=name)
+
+if (os.getenv('LIVE', 'false') == 'true'):
+    if __name__ == '__main__':
+        app.run(debug=False, host='0.0.0.0', port=80)
+else:
+    if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0')
